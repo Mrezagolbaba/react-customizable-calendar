3275b6eff7e027214a0d56928eaa8dedfcf95849
Mohamadreza Golbaba <r.golbaba@extratik.com>
2023-03-15 23:48:48 +0300

    header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+// Header.tsx
+import React from 'react';
+import { format } from 'date-fns';
+
+interface HeaderProps {
+  month: Date;
+  monthFormat: string;
+  yearFormat: string;
+}
+
+export const Header = ({
+  month,
+  monthFormat,
+  yearFormat,
+}: HeaderProps): JSX.Element => {
+  return (
+    <div className="rc-calendar__header">
+      <div className="rc-calendar__month-label">
+        {format(month, monthFormat)}
+      </div>
+      <div className="rc-calendar__year-label">
+        {format(month, yearFormat)}
+      </div>
+    </div>
+  );
+};
